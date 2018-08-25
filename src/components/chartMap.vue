943467f1f59ebc87893ef38cfebd1faa73534e42
rueen <rueenli@gmail.com>
2018-08-25 13:15:00 +0800

    build

--- src/components/chartMap.vue
+++ src/components/chartMap.vue
@@ -5,10 +5,10 @@
 <script>
 import echarts from 'echarts'
 import getElement from '../js/getElement.js'
-import hzmapdata from '../json/hangzhou.json'
+import whmapdata from '../json/371400.json'
 import data from '../json/data.json'
 
-echarts.registerMap('hangzhou', hzmapdata);
+echarts.registerMap('weihai', whmapdata);
 
 export default {
     data() {
@@ -24,30 +24,7 @@ export default {
             var option = {
                 tooltip: {
                     trigger: 'item',
-                    backgroundColor: 'rgba(3,72,123,1)',
-                    borderColor: '#03487b',
-                    borderRadius: 8,
-                    borderWidth: 2,
-                    padding: 10,    // [5, 10, 15, 20]
-                    position: function (p) {
-                        // 位置回调
-                        // console.log && console.log(p);
-                        return [p[0] + 10, p[1] - 10];
-                    },
-                    formatter: function (params, ticket, callback) {
-                        // console.log(params)
-                        // var res = 'Function formatter : <br/>' + params.name;
-                        // for (var i = 0, l = params.length; i < l; i++) {
-                        //     res += '<br/>' + params[i].seriesName + ' : ' + params[i].value;
-                        // }
-                        var obj = data[params.name];
-                        var res = '<div>' + params.name + '</div><div>企业：' + obj.enterprise + '</div><div>自然人：' + obj.people + '</div>'
-                        setTimeout(function () {
-                            // 仅为了模拟异步回调
-                            callback(ticket, res);
-                        }, 50);
-                        return res;
-                    }
+                    formatter: '{b}<br/>{c} (p / km2)'
                 },
                 legend: {
                     orient: 'vertical',
@@ -57,17 +34,27 @@ export default {
                     }
                 },
                 visualMap: {
-                    min: 0,
-                    max: 2500,
-                    show: false,
+                    min: 800,
+                    max: 50000,
+                    text:['High','Low'],
+                    realtime: false,
+                    calculable: true,
                     inRange: {
-                        color: ['#ff7980', '#ffb59a', '#ffd6b8']
-                    },
-                    formater: 'aaaa{1000-2000}'
+                        color: ['lightskyblue','yellow', 'orangered']
+                    }
                 },
+                // visualMap: {
+                //     min: 0,
+                //     max: 2500,
+                //     show: false,
+                //     inRange: {
+                //         color: ['#ff7980', '#ffb59a', '#ffd6b8']
+                //     },
+                //     formater: 'aaaa{1000-2000}'
+                // },
                 series: [{
                     type: 'map',
-                    map: 'hangzhou',
+                    map: 'weihai',
                     roam: false,
                     scaleLimit:{
                         min:1,
@@ -81,20 +68,10 @@ export default {
                             show: false
                         }
                     },
-                    itemStyle: {
-                        normal: {
-                            areaColor: '#0e2b43',
-                            borderColor: '#296bac'
-                        },
-                        emphasis:{
-                            label:{
-                                show:false
-                            },
-                            // areaColor:"#0186e1",
-                            color:'red',
-                            borderColor:'#48b'
-                        }
-                    }
+                    itemStyle:{
+                        normal:{label:{show:true}},
+                        emphasis:{label:{show:true}}
+                    },
                 }]
             };
             
